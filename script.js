e5eb5e53141ef21c9d8fb7ed4bdfca8fc84ed223
SK SINGH <singhsushil5277@gmail.com>
2024-08-29 12:38:29 +0545

    Updated CSS to all section

--- script.js
+++ script.js
@@ -54,7 +54,7 @@ function changeBackground() {
 }
 
 // Change the background image every 5 seconds (5000 milliseconds)
-setInterval(changeBackground, 5000);
+// setInterval(changeBackground, 5000);
 
 // document.addEventListener("DOMContentLoaded", function () {
 //   // Get all buttons and text contents
